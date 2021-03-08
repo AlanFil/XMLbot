56654a39112d214d9cef2f0272846c91382b7809
AlanFil <49269767+AlanFil@users.noreply.github.com>
2021-03-08 10:56:33 +0100

    Add files via upload

---
+++ FTP_connection/transfer.py
@@ -0,0 +1,62 @@
+import base64
+import ftplib
+import os
+from globals import STARTING_DIRECTORY
+
+
+def make_FTP_connection():
+    FTP_HOST = "5.252.230.94"
+    FTP_USER = "matrixmedia"
+
+    # (1) decode password
+    base64_message = 'WXpmOXkydU1zbnFldUtFYg=='
+    base64_bytes = base64_message.encode('ascii')
+    message_bytes = base64.b64decode(base64_bytes)
+    FTP_PASS = message_bytes.decode('ascii')
+
+    # (2) make connection
+    FTP = ftplib.FTP(FTP_HOST, FTP_USER, FTP_PASS)
+    FTP.encoding = "utf-8"
+
+    return FTP
+
+
+def close_FTP_connection(FTP):
+    FTP.quit()
+
+
+def send_description_imgs_via_FTP(FTP, brand, product_folder_name):
+    # move to proper folder in FTP
+    try:
+        FTP.mkd(f"/domains/matrixmedia.pl/public_html/current/files/products/{brand}")
+    except ftplib.error_perm:
+        print(f'Folder {brand} already exists')
+    finally:
+        FTP.cwd(f"/domains/matrixmedia.pl/public_html/current/files/products/{brand}")
+    try:
+        FTP.mkd(f"/domains/matrixmedia.pl/public_html/current/files/products/{brand}/{product_folder_name}")
+    except ftplib.error_perm:
+        print(f'Folder {product_folder_name} already exists')
+    finally:
+        FTP.cwd(f"/domains/matrixmedia.pl/public_html/current/files/products/{brand}/{product_folder_name}")
+
+    description_imgs_path = f'bin\\{brand} - {product_folder_name}\\description_imgs'
+    for filename in os.listdir(description_imgs_path):
+        with open(f'{description_imgs_path}\\{filename}', 'rb') as file:
+            FTP.storbinary("STOR " + filename, file)
+
+
+def send_XML_via_FTP(FTP, xml_file_name):
+    # sending an XML file
+    FTP.cwd('/domains/matrixmedia.pl/public_html/import_products/xmls')
+    with open(f'bin\\{xml_file_name}', 'rb') as file:
+        FTP.storbinary("STOR " + xml_file_name, file)
+
+
+def send_products_imgs_via_FTP(FTP, product_file_path):
+    # sending product imgs
+    FTP.cwd('/domains/matrixmedia.pl/public_html/import_products/pictures')
+    product_imgs_path = f'bin\\{product_file_path}\\product_imgs'
+    for filename in os.listdir(product_imgs_path):
+        with open(f'{product_imgs_path}\\{filename}', 'rb') as file:
+            FTP.storbinary("STOR " + filename, file)
